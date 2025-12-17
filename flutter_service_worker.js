89b313d6c413c8f6d688d3ca9ca842e317aa6062
braisIA <bdelafuentevilas@gmail.com>
2025-12-17 15:39:50 +0100

    Use ngrok backend

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "40d0a0e7e170b6d5f4df1f34849dea4a",
+"flutter_bootstrap.js": "8f51b42b802504b55317ef627adb7718",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "677b3b9598b320d0c529b3f7f6eb1403",
 "/": "677b3b9598b320d0c529b3f7f6eb1403",
-"main.dart.js": "786dc6a69a9a2e5eca19995361d44e57",
+"main.dart.js": "9737466cb94b35fc7d8af1ed69935dc7",
 "manifest.json": "bf24c84c3bf99672a631c4f84464e793",
 "version.json": "15235b5108d6a877ef74fe3317a96bf7"};
 // The application shell files that are downloaded before a service worker can
